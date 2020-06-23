802d2d9b361e92a6c1e5c8028448917f69d400a2
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-06-22 18:36:14 -0700

    editing bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,13 +28,61 @@ function displayProducts(){
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log("ITEM ID: " +res[i].item_id + " | " + "ITEM: " + res[i].product_name + " | " + "PRICE: " + res[i].price);
-            console.log("-----------------------------------");
+            console.log("-----------------------------------\n");
           };
-        });   
+
+          purchaseProduct(); 
+        });  
 };
 //prompt then asks ID of the product they would like to buy
-
 //prompt to ask the quantity
+function purchaseProduct(){
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Enter the ID of the item you would like to purchase: "
+    },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many of this item would you like to purchase? "
+    }
+]).then(function(response){
+    connection.query("SELECT * FROM products", function (err,res){
+        if (err) throw err;
+        var pickedItem;
+        for (var i = 0; i < res.length; i++) {
+            if(res[i].item_id == response.id){
+                pickedItem = res[i];
+            }
+          }
+        if(pickedItem.stock_quantity > parseInt(response.quantity)){
+        connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: (pickedItem.stock_quantity - (response.quantity))
+                },
+                {
+                    item_id: pickedItem.id
+                }
+            ],
+            function(error){
+                if(error) throw error;
+                console.log("Thank you for shopping at bamazon! Your total comes out to: $" + parseInt(response.quantity) * pickedItem.price);
+                connection.end();
+            } 
+        );
+        } else{
+            console.log("So sorry! We do not have enough stock to cover your needs!");
+            console.log("Please try again! We'd love for you to continue shopping with us!");
+            console.log("--------------------------------\n");
+            displayProducts();
+        }; 
+        }); 
+});
+};
+
 
 //check if there is enough quantity, if not, log "insufficient quanitity", error unfulfilled order
 
